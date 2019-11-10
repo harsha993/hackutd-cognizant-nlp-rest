9069c7dee70fc06b0b8ded5812c15227a10a8b4d
Harshavardhan <harshavardhanr.993@gmail.com>
2019-11-10 07:36:03 -0600

    Added more settings to enable integration with heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn hackutdrest.wsgi
--- hackutdrest/settings.py
+++ hackutdrest/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -123,6 +123,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = "app-root/repo/wsgi/static"
 CORS_ORIGIN_ALLOW_ALL = True
+# Activate django_heroku
+django_heroku.settings(locals())
 
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:8000',
